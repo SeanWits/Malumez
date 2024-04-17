82c864f7446761b5d2a9a7b4f9f8c61cbe675f0a
SeanMz28 <2438634@students.wits.ac.za>
2024-04-17 11:08:19 +0200

    Basic Sign up Functionality done

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import SignUp  from './pages/signUp';
+import Login from './pages/login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
 
