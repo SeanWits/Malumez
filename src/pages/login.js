6de101626024fdfb7dd9c2c2367fdb83942c2245
ValerieC123 <102948171+ValerieC123@users.noreply.github.com>
2024-04-18 21:51:40 +0200

    Signup and Login page formatted so signup page displays as a popup. Logo added to log in page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 function openPopup() {
     //window.open("signup.html",popup=yes, rel="noreferrer", "Signup Popup", "width=500,height=700");
-    window.open("signup.html", "Signup Popup", "width=500,height=700,left=" + ((window.screen.width - 500) / 2) + ",top=" + ((window.screen.height - 700) / 2) + ",toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,copyhistory=no,resizable=no");
+    window.open("signup.html", "Signup Popup", "width=500,height=700,left=" + ((window.screen.width - 500) / 2) + ",top=" + ((window.screen.height - 700) / 2) + ",toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=0,copyhistory=0,resizable=0");
 
 }
