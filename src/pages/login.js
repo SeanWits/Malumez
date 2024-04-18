65c0e20fce550a73b23ca2379e2c0d8c2ff59da0
ValerieC123 <102948171+ValerieC123@users.noreply.github.com>
2024-04-18 17:47:14 +0200

    Modified the frontend for the log in page and added the frontend for the signup screen. The signup screen comes in as a popup when the signup button on the login screen in pressed

---
+++ src/pages/login.js
@@ -0,0 +1,5 @@
+function openPopup() {
+    //window.open("signup.html",popup=yes, rel="noreferrer", "Signup Popup", "width=500,height=700");
+    window.open("signup.html", "Signup Popup", "width=500,height=700,left=" + ((window.screen.width - 500) / 2) + ",top=" + ((window.screen.height - 700) / 2) + ",toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,copyhistory=no,resizable=no");
+
+}
