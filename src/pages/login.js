11f99d931da5e8f74e6e1cc8115fe4e11a36bca5
R2theextreme <rofunwa16@gmail.com>
2024-04-19 12:39:05 +0200

    Changes made to login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,11 @@
+import { auth } from '../firebase';
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 function openPopup() {
     //window.open("signup.html",popup=yes, rel="noreferrer", "Signup Popup", "width=500,height=700");
     window.open("signup.html", "Signup Popup", "width=500,height=700,left=" + ((window.screen.width - 500) / 2) + ",top=" + ((window.screen.height - 700) / 2) + ",toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=0,copyhistory=0,resizable=0");
 
 }
-import { auth } from '../firebase';
-import React, { useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
 
 function Login() {
     const [email, setEmail] = useState('');
