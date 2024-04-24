7cdba69397b56655dd5131d1a6f08af4a7588361
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com.>
2024-04-24 11:31:14 +0200

    Added featured products section. Added category sections.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./assets/Malume'zLogoFullNoBackground.png";
 import './App.css';
 
-function Header() {
+export function Header() {
   return (
     <>
     <head>
@@ -26,16 +26,68 @@ export function SearchBar()
   return (
     <>
       <section className="searchBar">
-        <button type ="button" id="search_options" className="options_button" />
-        <section>
-          <input type="text" placeholder="Search.."/>
-          <button type ="button" id="search_options" className="options_button" />
+          <i className='fa fa-bars icon'/>
+        {/* <button type ="button" id="search_options" className="options_button" /> */}
+        <section className="search">
+          <input className="inputSearch" type="text" placeholder="Search.."/>
+          <i className='fa fa-search icon'/>
+          {/* <button type ="button" id="search_options" className="options_button" /> */}
         </section>
       </section>
     </>
   )
 }
 
+export function AdsBar()
+{
+  let adImage=require("./assets/Malume'zLogoFull.png");
+  return (
+    <>
+      <section className="adsBar">
+        <i className="fa fa-chevron-left icon"></i>
+        <img className="adsImage" src={adImage} alt="Image of an Ad"></img>
+        <i className="fa fa-chevron-right icon"></i>
+      </section>
+    </>
+  )
+}
+
+export function FeaturedProducts()
+{
+  let brandImage=require("./assets/Malume'zLogoFull.png");
+
+  return (
+    <>
+      <section  className="featuredProducts">
+        <i className="fa fa-chevron-left icon"></i>
+        <img className="brandImage" src={brandImage} alt="Image of a featured product"></img>
+        <img className="brandImage" src={brandImage} alt="Image of a featured product"></img>
+        <img className="brandImage" src={brandImage} alt="Image of a featured product"></img>
+        <i className="fa fa-chevron-right icon"></i>
+      </section>
+    </>
+  )
+}
+
+export function Categories()
+{
+  let productImage=require("./assets/Malume'zLogoFull.png");
+  let categoryName = "Category"
+  return (
+    <>
+      <section className="categorySection">
+        <h2 className="categoryHeading">
+          {categoryName}
+        </h2>
+        <section className="productSection">
+          <img className="productImage" src={productImage} alt="Image of product" />
+          <img className="productImage" src={productImage} alt="Image of product" />
+          <img className="productImage" src={productImage} alt="Image of product" />
+        </section>
+      </section>
+    </>
+  )
+
+}
 //Wassup
 
-export default Header;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './App';
+import { Header } from './App';
 import { SearchBar } from "./App"
+import { AdsBar } from './App';
+import { FeaturedProducts } from './App';
+import { Categories } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
     <SearchBar />
+    <AdsBar />
+    <FeaturedProducts />
+    <Categories />
   </React.StrictMode>
 );
 
