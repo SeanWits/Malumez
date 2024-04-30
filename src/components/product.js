84ce6abb74e02c2dfce68f02c5d1a9605ceddcb5
SeanMz28 <2438634@students.wits.ac.za>
2024-04-30 11:53:05 +0200

    Added cart functionality and link to Check Out.

--- src/components/product.js
+++ src/components/product.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './product.css';
 
-const Product = ({ imageUrl, price, name }) => {
+const Product = ({ imageUrl, price, name, onAddToCart, onRemoveFromCart }) => {
   return (
-    <div className="product">
-      <img src={imageUrl} alt={name} />
-      <div className="product-info">
-        <h2>{name}</h2>
-        <p>R{price}</p>
+      <div className="product">
+          <img src={imageUrl} alt={name} />
+          <div className="product-info">
+              <h2>{name}</h2>
+              <p>R{price}</p>
+              <div className="button-container">
+                  <button className="add-to-cart-btn" onClick={onAddToCart}>+</button>
+                  <button className="remove-from-cart-btn" onClick={onRemoveFromCart}>-</button>
+              </div>
+          </div>
       </div>
-    </div>
   );
 };
 
