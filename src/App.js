bb4346e85484ccd5a089594e2df7936d00f5b4bd
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com.>
2024-04-30 11:01:45 +0200

    hid App.css and admin page import

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
+// import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
 import SignUp from './pages/signUp';
-import Admin from './pages/admin';
+// import Admin from './pages/admin';
 import ShowProducts from './components/showProducts';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/home" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
-        <Route path="/" element={<Home/>}/>
         <Route path="/signUp" element={<SignUp/>}/>
-        <Route path="/admin" element={<Admin/>}/>
+        {/* <Route path="/admin" element={<Admin/>}/> */}
         <Route path="/showProducts" element={<ShowProducts/>}/>
       </Routes>
     </Router>
