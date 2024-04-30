ec31b682a137803b9c63e585b6ffee41f2119550
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com.>
2024-04-30 10:21:35 +0200

    deleted imports from home.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import { Header } from './pages/home';
-import { SearchBar } from "./pages/home"
-import { AdsBar } from './pages/home';
-import { FeaturedProducts } from './pages/home';
-import { Categories } from './pages/home';
-import { MoreOptions } from './pages/home';
-import { Footer } from './pages/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
