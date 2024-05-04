8ba36fc8da376347625cad7173aa6b88f0aa0052
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com.>
2024-05-04 21:49:59 +0200

    removed body element from header

--- src/components/Seller/Header.js
+++ src/components/Seller/Header.js
@@ -13,24 +13,22 @@ export function Header() {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
       </head>
-      <body>
-        <header className="homeHeader">
-          <img
-            onClick={() => navigate("/")}
-            src={logo}
-            alt="Malume'z Logo"
-            height="60"
-            width="auto"
+      <header className="homeHeader">
+        <img
+          onClick={() => navigate("/")}
+          src={logo}
+          alt="Malume'z Logo"
+          height="60"
+          width="auto"
+        />
+        <section>
+          <i
+            onClick={() => navigate("/uploadImg")}
+            className="fa fa-upload icon"
           />
-          <section>
-            <i
-              onClick={() => navigate("/uploadImg")}
-              className="fa fa-upload icon"
-            />
-            <i onClick={() => navigate("/login")} className="fa fa-bell icon" />
-          </section>
-        </header>
-      </body>
+          <i onClick={() => navigate("/login")} className="fa fa-bell icon" />
+        </section>
+      </header>
     </>
   );
 }
