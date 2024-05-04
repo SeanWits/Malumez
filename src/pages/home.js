85d0c096cf1127e640c601bddc5787968db39ab4
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com.>
2024-05-04 18:59:02 +0200

    created seperate file for FeaturedProducts function

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,29 +1,13 @@
-import logo from "../assets/Malume'zLogoFullNoBackground.png";
+// import logo from "../assets/Malume'zLogoFullNoBackground.png";
 import './home.css';
-import { Router, useNavigate } from 'react-router-dom';
-import { Header } from "../components/Header";
-import { SearchBar } from "../components/Search";
-import { MoreOptions } from "../components/More_Options";
-import { Footer } from "../components/Footer";
-import { Categories } from "../components/Categories";
-import { AdsBar } from "../components/AdsBar";
-
-export function FeaturedProducts()
-{
-  let brandImage=require("../assets/Malume'zLogoFull.png");
-
-  return (
-    <>
-      <section  className="featuredProducts">
-        <i className="fa fa-chevron-left icon left"></i>
-        <img className="brandImage" src={brandImage} alt="Image of a featured product"></img>
-        <img className="brandImage" src={brandImage} alt="Image of a featured product"></img>
-        <img className="brandImage" src={brandImage} alt="Image of a featured product"></img>
-        <i className="fa fa-chevron-right icon right"></i>
-      </section>
-    </>
-  )
-}
+// import { Router, useNavigate } from 'react-router-dom';
+import { Header } from "../components/Home/Header";
+import { SearchBar } from "../components/Home/Search";
+import { MoreOptions } from "../components/Home/More_Options";
+import { Footer } from "../components/Home/Footer";
+import { Categories } from "../components/Home/Categories";
+import { AdsBar } from "../components/Home/AdsBar";
+import { FeaturedProducts } from "../components/Home/FeaturedProduct";
 
 //Wassup
 
