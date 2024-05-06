d8d6c7adf8d204591502e92ee26e4aaa7b61decf
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com.>
2024-05-06 16:54:20 +0200

    added stock function and footer to seller page

--- src/pages/Seller.js
+++ src/pages/Seller.js
@@ -1,10 +1,14 @@
 // import "./Seller.css";
 import { Header } from "../components/Seller/Header";
+import { Stock } from "../components/Seller/Stock";
+import { Footer } from "../components/Home/Footer";
 
 function Seller() {
   return (
     <>
       <Header />
+      <Stock />
+      <Footer />
     </>
   );
 }
