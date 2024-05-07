c94dd6bd272af5b9f8a4ec5b085b5fedc02dadcd
SeanMz28 <2438634@students.wits.ac.za>
2024-05-07 19:14:19 +0200

    Cart Functionity part 1

--- src/pages/products.js
+++ src/pages/products.js
@@ -3,14 +3,16 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../components/product';
 import { db } from '../firebase';
-import { getDocs, collection, query } from "firebase/firestore";
-import { useNavigate } from 'react-router-dom';
+import { getDocs, collection, query, doc, updateDoc, arrayUnion} from "firebase/firestore";
+import { useNavigate, useLocation } from 'react-router-dom';
 import { SearchBar } from '../components/Home/Search';
 import './products.css';
 import './home';
 import { Header } from "../components/Home/Header";
 import { Footer } from "../components/Home/Footer";
 import { MoreOptions } from '../components/Home/More_Options';
+import { auth } from '../firebase';
+//import { brands } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
@@ -58,11 +60,39 @@ const Products = () => {
         setCart(savedCart);
     }, []);
 
-    const addToCart = (product) => {
-        const updatedCart = [...cart, product];
-        setCart(updatedCart);
-        localStorage.setItem('cart', JSON.stringify(updatedCart));
-    };
+  
+
+ const addToCart = (product) => {
+    const updatedCart = [...cart, product];
+    setCart(updatedCart);
+    console.log(cart);
+  
+    // Get the current user
+    const currentUser = auth.currentUser;
+  
+    // Check if a user is logged in
+    if (currentUser) {
+        // Get the UID of the current user
+        const userId = currentUser.uid;
+  
+        // Retrieve the user document from Firestore
+        const userRef = doc(db, "users", userId);
+  
+        // Update the user document by adding the product ID to the cart array
+        updateDoc(userRef, {
+            cart: arrayUnion(product.id)
+        })
+        .then(() => {
+            console.log("Product added to cart successfully!");
+        })
+        .catch((error) => {
+            console.error("Error adding product to cart: ", error);
+        });
+    } else {
+        console.log("No user is logged in.");
+    }
+};
+
 
     const removeFromCart = (productId) => {
         const updatedCart = cart.filter(item => item.id !== productId);
@@ -70,9 +100,135 @@ const Products = () => {
         localStorage.setItem('cart', JSON.stringify(updatedCart));
     };
 
-    const handleCheckout = () => {
-        navigate('/checkout');
-    };
+const handleCheckout = () => {
+    navigate('/checkOut',{ state: cart });
+    console.log("Cart items:", cart);
+};
+
+const handleOptionChange = (event) => {
+  setSelectedOption(event.target.value);
+};
+
+// searching for products based on user inputs
+// if(searchItem !== null)
+//   {
+//     products.forEach(product=>{
+//       if(product.category === searchItem || product.brand === searchItem || product.name === searchItem)
+//         {
+          
+//           console.log(product);
+//             filtered[i]=product;
+//             i++;
+//         }
+//       });
+//       if(filtered.length === 0)
+//         {
+//           console.log("Product could not be found");
+
+//         }
+
+//         i=0;
+     
+//   }
+// filtering the products by what the user selects
+function applyFilters()
+{
+   let category = document.getElementById("categoriesDropdown").value;
+   let brand = document.getElementById("brandsDropdown").value;
+   console.log(searchItem);
+   console.log(category);
+   console.log(brand);
+   console.log(selectedOption);
+
+   // everytime the filter gets called, it must clear all the previous filter applications
+   let storeProducts = []
+
+  if(category !== "all" && brand!== 'all' ) // i.e there is a value for both categories and brands
+    {
+      
+      products.forEach(product=>{
+        if(product.category === category && product.brand === brand)
+          {
+            
+            console.log(product);
+              storeProducts[i]=product;
+              i++;
+          }});
+      
+
+    }
+    else if(category !== "all" && brand=== 'all')// there is a category but no brand
+    {
+      
+      products.forEach(product=>{
+        if(product.category === category)
+          {
+            
+            console.log(product);
+              storeProducts[i]=product;
+              i++;
+          }
+
+       });
+
+
+    }
+    else if(category === "all" && brand !== "all")// brand with no category
+    {
+      
+      products.forEach(product=>{
+        if(product.brand === brand)
+          {
+            
+            console.log(product);
+              storeProducts[i]=product;
+              i++;
+          }
+
+       });
+
+    }
+    else if(category === "all" && brand=== "all")// no category and no brand
+    {
+      
+      products.forEach(product=>{
+            console.log(product);
+              storeProducts[i]=product;
+              i++;
+       });
+    }
+
+    // organise products based on price - Low to high or high to low
+    if(selectedOption === "highToLow")
+    {
+      // sorting from high to low price
+      storeProducts.sort((a, b) => b.price - a.price);
+      
+    }
+    else if(selectedOption === "lowToHigh")
+    {
+      // Sorting from low to high price
+      storeProducts.sort((a, b) => a.price - b.price);
+
+    }
+    
+    if (storeProducts.length > 0) {
+      setFiltered(storeProducts);
+      setProductsFiltered(true);
+    }
+    i = 0;
+  
+}
+
+
+
+
+
+
+// check if the brands have actually been retrieved
+
+
+
 
     return (
         <>
