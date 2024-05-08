a81b170ad6af1f33a6df3016c24ce11f4994780e
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-08 14:08:50 +0200

    created OrderStatus page

---
+++ src/components/Buyer/OrderStatus.js
@@ -0,0 +1,12 @@
+import { Header } from "../Home/Header"
+import { Footer } from "../Home/Footer"
+
+export default function OrderStatus(){
+    return (
+        <>
+        <Header />
+        <section className="orderList"></section>
+        <Footer />
+        </>
+    )
+}
