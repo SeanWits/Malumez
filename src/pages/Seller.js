24e5dfb363b5cbf4cfacb9bbb221445c5f9e5278
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-08 14:38:13 +0200

    changed call from Stock to SellerProducts

--- src/pages/Seller.js
+++ src/pages/Seller.js
@@ -1,6 +1,7 @@
 // import "./Seller.css";
 import { Header } from "../components/Seller/Header";
-import { Stock } from "../components/Seller/Stock";
+// import { Stock } from "../components/Seller/Stock";
+import SellerProducts from "../components/Seller/SellerProducts";
 import { Footer } from "../components/Home/Footer";
 import { ShopDetails } from "../components/Seller/ShopDetails";
 import "./Seller.css"
@@ -11,7 +12,7 @@ function Seller() {
       <Header />
       <section className="details_stock">
         <ShopDetails />
-        <Stock />
+        <SellerProducts />
       </section>
       <Footer />
     </>
