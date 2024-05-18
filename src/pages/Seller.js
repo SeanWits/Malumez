36b63a87c64555cff4354c356111b22031e8ef05
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-18 15:00:51 +0200

    deleted comments

--- src/pages/Seller.js
+++ src/pages/Seller.js
@@ -1,22 +1,20 @@
-// import "./Seller.css";
 import { Header } from "../components/Seller/Header";
-// import { Stock } from "../components/Seller/Stock";
 import SellerProducts from "../components/Seller/SellerProducts";
 import { Footer } from "../components/Home/Footer";
 import { ShopDetails } from "../components/Seller/ShopDetails";
-import "./Seller.css"
+import "./Seller.css";
 
 function Seller() {
-  return (
-    <>
-      <Header />
-      <section className="details_stock">
-        <ShopDetails />
-        <SellerProducts />
-      </section>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <section className="details_stock">
+                <ShopDetails />
+                <SellerProducts />
+            </section>
+            <Footer />
+        </>
+    );
 }
 
 export default Seller;
