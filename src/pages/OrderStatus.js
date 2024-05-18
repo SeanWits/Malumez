df149a1dc530f14f938b9b220fe939d4d925eea5
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-18 23:30:27 +0200

    called order tacking section on order status page

--- src/pages/OrderStatus.js
+++ src/pages/OrderStatus.js
@@ -1,10 +1,12 @@
 import { Header } from "../components/Home/Header";
 import { Footer } from "../components/Home/Footer";
 import { MoreOptions } from "../components/Home/More_Options";
+import OrderTracking from "../components/OrderStatus/OrderTracking";
 export default function OrderStatus() {
     return (
         <>
             <Header />
+            <OrderTracking />
             <MoreOptions />
             <Footer />
         </>
