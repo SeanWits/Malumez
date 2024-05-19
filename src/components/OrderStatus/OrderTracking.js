c8d1dcc004c4f8645a61a5c22562e67450210828
ValerieC123 <102948171+ValerieC123@users.noreply.github.com>
2024-05-19 09:45:55 +0200

    Page created that displays all orders - This page needs to link to the other pages

--- src/components/OrderStatus/OrderTracking.js
+++ src/components/OrderStatus/OrderTracking.js
@@ -1,7 +1,14 @@
 import "./OrderTracking.css";
+import { useEffect, useState } from "react";
+//import { db } from '../firebase'; 
 
 export function OrderTracking() {
+    const [order, setOrder] = useState();
+
+
     const orderConfirmationStatus = "Pending Confirmation";
+    // the order/ cart the person has clicked should be populated from here
+
     return (
         <>
             <article className="OrderTrackingArticle">
@@ -9,6 +16,7 @@ export function OrderTracking() {
                     <section className="OrderStatusHeadingSection">
                         <h2 className="OrderStatusHeading">Order Status</h2>
                     </section>
+
                     <section className="OrderProgressionSection">
                         <section className="OrderProgressionStatusSection">
                             <div className="square" id="orderSubmitted">
@@ -16,28 +24,33 @@ export function OrderTracking() {
                                     Order Submitted
                                 </p>
                             </div>
+
                             <i className="fa fa-long-arrow-right icon" />
                             <div className="square" id="orderConfirmation">
                                 <p className="OrderStatusText">
                                     {orderConfirmationStatus}
                                 </p>
                             </div>
+
                             <i className="fa fa-long-arrow-right icon" />
                             <div className="square" id="orderAvailable">
                                 <p className="OrderStatusText">
                                     Order Available for collection
                                 </p>
                             </div>
+
                             <i className="fa fa-long-arrow-right icon" />
                             <div className="square" id="orderCollected">
                                 <p className="OrderStatusText">
                                     Order Collected
                                 </p>
                             </div>
+
                             <i className="fa fa-long-arrow-right icon" />
                             <div className="square" id="orderClosed">
                                 <p className="OrderStatusText">Order Closed</p>
                             </div>
+
                         </section>
                         <section className="OrderProgressionSection">
                             <section className="OrderStatusHeadingSection">
