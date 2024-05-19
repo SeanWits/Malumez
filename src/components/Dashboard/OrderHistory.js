7f354a23c382867721c5d51817802700e5ec5ea5
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-19 20:38:19 +0200

    added order history component

---
+++ src/components/Dashboard/OrderHistory.js
@@ -0,0 +1,36 @@
+export default function OrderHistory() {
+    return (
+        <>
+            {/* OrderHistoryArticle: block for list of previous orders */}
+            <article className="OrderHistoryArticle">
+                {/* OrderHistoryArticle heading */}
+                <section className="Heading OrderHistorySectionHeading">
+                    <h2 className="HeadingText OrderHistoryHeadingText">
+                        Order History
+                    </h2>
+                </section>
+                {/* Order History List */}
+                <section className="OrderHistoryEntrySection">
+                    <ul className="OrderHistoryEntryList">
+                        <li id="OrderNumber">
+                            Order Number:
+                            {/* Enter the order number that is fetched from the database here */}
+                        </li>
+                        <li id="OrderDate">
+                            Order Date:
+                            {/* Enter the order date that is fetched from the database here */}
+                        </li>
+                        <li id="OrderLocation">
+                            Order Location:
+                            {/* Enter the order location that is fetched from the database here */}
+                        </li>
+                        <li id="OrderTotal">
+                            Order Number:
+                            {/* Enter the order number that is fetched from the database here */}
+                        </li>
+                    </ul>
+                </section>
+            </article>
+        </>
+    );
+}
