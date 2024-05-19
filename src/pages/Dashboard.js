d1c35644e18765f9b48b6e774965a809484cb8d1
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-19 20:38:28 +0200

    added logout button

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,12 +2,26 @@ import { Header } from "../components/Home/Header";
 import { Footer } from "../components/Home/Footer";
 import { MoreOptions } from "../components/Home/More_Options";
 import UserDetails from "../components/Dashboard/UserDetails";
-
+import { UserContext } from "../App";
+import { useContext } from "react";
+import OrderHistory from "../components/Dashboard/OrderHistory";
+import Checkout from "./Checkout";
 export default function Dashboard() {
+    const user = useContext(UserContext);
     return (
         <>
             <Header />
-            <UserDetails />
+            <section className="Dashboard">
+                <UserDetails user={user} />
+                <OrderHistory />
+                <button
+                    type="button"
+                    id="logout"
+                    onClick={Checkout.handleSignOut}
+                >
+                    Logout
+                </button>
+            </section>
             <MoreOptions />
             <Footer />
         </>
