7a0f7c30f3369a8c858a2253aa2c487471f41f0a
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-19 20:39:50 +0200

    passed user to header

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,28 @@
 // import logo from "../assets/Malume'zLogoFullNoBackground.png";
-import './home.css';
+import "./home.css";
 import { Header } from "../components/Home/Header";
 import { SearchBar } from "../components/Home/Search";
 import { MoreOptions } from "../components/Home/More_Options";
 import { Footer } from "../components/Home/Footer";
 import { Categories } from "../components/Home/Categories";
 import { AdsBar } from "../components/Home/AdsBar";
-import { FeaturedProducts } from '../components/Home/FeaturedProduct';
+import { FeaturedProducts } from "../components/Home/FeaturedProduct";
+import { UserContext } from "../App";
+import { useContext } from "react";
 
-function Home() 
-{
-  return (
-  <>
-    <Header />
-    <SearchBar />
-    <AdsBar />
-    <FeaturedProducts />
-    <Categories />
-    <MoreOptions />
-    <Footer />  
-  </>
-  )
+function Home() {
+    const user = useContext(UserContext);
+    return (
+        <>
+            <Header user={user} />
+            <SearchBar />
+            <AdsBar />
+            <FeaturedProducts />
+            <Categories />
+            <MoreOptions />
+            <Footer />
+        </>
+    );
 }
 
 export default Home;
