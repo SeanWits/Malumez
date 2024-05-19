ee90f85bbf3b2cf42c10224150a80c5541daa1fc
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-19 20:39:07 +0200

    imported user from home page

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,18 +1,23 @@
 import logo from "../../assets/Malume'zLogoFullNoBackground.png";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
-export function Header() {
+export function Header({user}) {
     const navigate = useNavigate();
     const removeBodyClass = (className) =>
         document.body.classList.remove(className);
     removeBodyClass("bodyHidden");
+    let route = "/login";
+    console.log(user);
+    if (user) {
+        route = "/dashboard";
+    }
 
     return (
         <>
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-                />
+            <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+            />
             <header className="homeHeader">
                 <img
                     onClick={() => navigate("/")}
@@ -22,7 +27,11 @@ export function Header() {
                     width="auto"
                 />
                 <section>
-                <i className="fa fa-archive" id = "orderStatusIcon" onClick={()=>navigate('/OrderStatus')}></i>
+                    <i
+                        className="fa fa-archive"
+                        id="orderStatusIcon"
+                        onClick={() => navigate("/OrderStatus")}
+                    ></i>
 
                     <i
                         onClick={() => navigate("/seller")}
@@ -33,7 +42,7 @@ export function Header() {
                         className="fa fa-shopping-basket icon"
                     />
                     <i
-                        onClick={() => navigate("/login")}
+                        onClick={() => navigate(route)}
                         className="fa fa-user-circle icon"
                     />
                 </section>
