877e4cf5284cb6d3f90dca06111b5167442ac782
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-20 10:32:06 +0200

    undid the changes that were made to seller page

--- src/components/Seller/Header.js
+++ src/components/Seller/Header.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "../../assets/Malume'zLogoFullNoBackground.png";
 
 const Header = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <header className="homeHeader">
-      <img
-        onClick={() => navigate("/")}
-        src={logo}
-        alt="Malume'z Logo"
-        height="60"
-      />
-      <section>
-        <button aria-label="upload" onClick={() => navigate('/uploadImg')}>
-          <i className="fa fa-upload icon" />
-        </button>
-        <button aria-label="bell" onClick={() => navigate('/login')}>
-          <i className="fa fa-bell icon" />
-        </button>
-      </section>
-    </header>
-  );
+    return (
+        <header className="homeHeader">
+            <img
+                onClick={() => navigate("/")}
+                src={logo}
+                alt="Malume'z Logo"
+                height="60"
+            />
+            <section>
+                <i
+                    className="fa fa-upload icon"
+                    onClick={() => navigate("/uploadImg")}
+                />
+                <i
+                    className="fa fa-bell icon"
+                    onClick={() => navigate("/notifications")}
+                />
+            </section>
+        </header>
+    );
 };
 
 export default Header;
