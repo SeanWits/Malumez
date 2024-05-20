3fc678aab008a72205265dba20239d8e220096d4
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-20 10:37:22 +0200

    fixed icon invisibility

--- src/components/Seller/Header.js
+++ src/components/Seller/Header.js
@@ -1,29 +1,36 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/Malume'zLogoFullNoBackground.png";
+import "../../pages/home.css";
 
 const Header = () => {
     const navigate = useNavigate();
 
     return (
-        <header className="homeHeader">
-            <img
-                onClick={() => navigate("/")}
-                src={logo}
-                alt="Malume'z Logo"
-                height="60"
+        <>
+            <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
             />
-            <section>
-                <i
-                    className="fa fa-upload icon"
-                    onClick={() => navigate("/uploadImg")}
+            <header className="homeHeader">
+                <img
+                    onClick={() => navigate("/")}
+                    src={logo}
+                    alt="Malume'z Logo"
+                    height="60"
                 />
-                <i
-                    className="fa fa-bell icon"
-                    onClick={() => navigate("/notifications")}
-                />
-            </section>
-        </header>
+                <section>
+                    <i
+                        className="fa fa-upload icon"
+                        onClick={() => navigate("/uploadImg")}
+                    />
+                    <i
+                        className="fa fa-bell icon"
+                        onClick={() => navigate("/notifications")}
+                    />
+                </section>
+            </header>
+        </>
     );
 };
 
