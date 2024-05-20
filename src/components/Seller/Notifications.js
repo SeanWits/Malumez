54f78e81738a37e3ca147038883ea1ad11486c57
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-20 13:31:11 +0200

    implemented css for notifications page

--- src/components/Seller/Notifications.js
+++ src/components/Seller/Notifications.js
@@ -1,17 +1,24 @@
 import Header from "./Header";
 import React, { useEffect, useState, useContext } from "react";
 import { Footer } from "../Home/Footer";
-import { UserContext } from '../../App';
+import { UserContext } from "../../App";
 import { db } from "../../firebase";
-import { getDocs, query, collection, where, updateDoc, doc } from "firebase/firestore";
+import {
+    getDocs,
+    query,
+    collection,
+    where,
+    updateDoc,
+    doc,
+} from "firebase/firestore";
 import "./Notifications.css";
-import { format } from 'date-fns';
+import { format } from "date-fns";
 
 function OrderNotification({ order, shopId, fetchOrders }) {
     const updateProductStatus = async (orderId, productId, newStatus) => {
         try {
             const orderDocRef = doc(db, "orders", orderId);
-            const updatedItems = order.items.map(item => 
+            const updatedItems = order.items.map((item) =>
                 item.id === productId ? { ...item, status: newStatus } : item
             );
 
@@ -25,29 +32,62 @@ function OrderNotification({ order, shopId, fetchOrders }) {
 
     return (
         <>
-            <h3 className="orderHeading">Order - {order.username}</h3>
-            <section className="orderInfoSection">
-                <section className="orderDetails">
-                    <p>Order Date: {order.dateOrdered ? format(order.dateOrdered.toDate(), 'dd MMM yyyy, HH:mm:ss') : 'N/A'}</p>
-                    <p>Total Price: R{order.total}</p>
+            <section className="orderNotificationItem DashboardArticles">
+                <section className="orderHeadingSection Heading">
+                    <h3 className="orderHeading">Order - {order.username}</h3>
                 </section>
-                <section className="orders">
-                    <ul className="orderDetailList">
-                        {order.items.filter(item => item.shop_id === shopId).map(product => (
-                            <li key={product.id} className="orderItem">
-                                <span>{product.name} - R{product.price} - {product.status}</span>
-                                <select
-                                    value={product.status}
-                                    onChange={(e) => updateProductStatus(order.id, product.id, e.target.value)}
-                                >
-                                    <option value="ordered">Ordered</option>
-                                    <option value="packing">Packing</option>
-                                    <option value="ready_to_collect">Ready to Collect</option>
-                                    <option value="cancelled">Cancelled</option>
-                                </select>
+                <section className="orderInfoSection">
+                    <section className="orderDetails">
+                        <ul className="orderDetailsList">
+                            <li className="orderListItem">
+                                Order Date:{" "}
+                                {order.dateOrdered
+                                    ? format(
+                                          order.dateOrdered.toDate(),
+                                          "dd MMM yyyy, HH:mm:ss"
+                                      )
+                                    : "N/A"}
                             </li>
-                        ))}
-                    </ul>
+                            <li className="orderListItem">Total Price: R{order.total}</li>
+                        </ul>
+                    </section>
+                    <section className="orderDetails">
+                        <ul className="orderDetailsList">
+                            {order.items
+                                .filter((item) => item.shop_id === shopId)
+                                .map((product) => (
+                                    <li key={product.id} className="orderListItem orderItem">
+                                        <span className="orderSpan">
+                                            {product.name} - R{product.price} -{" "}
+                                            {product.status}
+                                        </span>
+                                        <select
+                                            value={product.status}
+                                            onChange={(e) =>
+                                                updateProductStatus(
+                                                    order.id,
+                                                    product.id,
+                                                    e.target.value
+                                                )
+                                            }
+                                        >
+                                            <option value="ordered">
+                                                Ordered
+                                            </option>
+                                            <option value="packing">
+                                                Packing
+                                            </option>
+                                            <option value="ready_to_collect">
+                                                Ready to Collect
+                                            </option>
+                                            <option value="cancelled">
+                                                Cancelled
+                                            </option>
+                                        </select>
+                                    </li>
+                                ))}
+                        </ul>
+                    </section>
                 </section>
             </section>
         </>
@@ -101,7 +141,10 @@ export default function Notifications() {
                 )
             );
 
-            const ordersData = ordersQuerySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            const ordersData = ordersQuerySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
             setOrders(ordersData);
             console.log("Orders fetched: ", ordersData);
         } catch (error) {
@@ -112,12 +155,12 @@ export default function Notifications() {
     return (
         <>
             <Header />
-            <section className="notificationHeadingSection">
+            <section className="notificationHeadingSection Heading">
                 <h2 className="notificationHeading">Notifications</h2>
             </section>
             <section className="orderNotification">
                 {orders.length > 0 ? (
-                    orders.map(order => (
+                    orders.map((order) => (
                         <OrderNotification
                             key={order.id}
                             order={order}
